80b17643194db2cef6202fc7aeb20f1ef6b19619
Thisw <90311899+thisw2004@users.noreply.github.com>
2024-04-15 21:08:51 +0200

    V1.3.2 - Can login,but redirect not fixed (error in blazor gui)

---
+++ maui/Components/Models/LoginModel.cs
@@ -0,0 +1,8 @@
+﻿namespace maui.Components.Models;
+
+public class LoginModel
+{
+    public string Email { get; set; }
+    public string Password { get; set; }
+}
+
---
+++ maui/Components/ViewModels/LoginViewModel.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Net.Http;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components;
+
+
+
+namespace maui.components.ViewModels
+{
+    public class LoginViewModel
+    {
+        [Inject]
+        private NavigationManager NavigationManager { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+
+        public event EventHandler<string> OnLoginSuccessful; // Event for successful login
+
+        public async Task<string> Login(NavigationManager navigationManager)
+        {
+            using (var httpClient = new HttpClient())
+            {
+                var loginDto = new { Username = Email, Password };
+                var json = JsonSerializer.Serialize(loginDto);
+                var content = new StringContent(json, Encoding.UTF8, "application/json");
+
+                var response = await httpClient.PostAsync("http://localhost:5041/api/account/login", content);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    var jsonResponse = await response.Content.ReadAsStringAsync();
+                    // Handle successful login response (e.g., deserialize data)
+                    // You may want to consider storing authentication details securely
+
+                    OnLoginSuccessful?.Invoke(this, jsonResponse); // Raise event for successful login
+                    
+                    
+                        navigationManager.NavigateTo("/home");
+                  
+                    //redirect here
+                    
+                    return jsonResponse; // Optional: Return response data for further processing
+                }
+                else
+                {
+                    return "Invalid login credentials. Please try again."; // Generic error message
+                }
+            }
+        }
+    }
+}
