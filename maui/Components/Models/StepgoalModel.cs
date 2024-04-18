2ca25ea3694f722a6d520df40f46dd2b847d1c0c
Thisw <90311899+thisw2004@users.noreply.github.com>
2024-04-18 09:20:26 +0200

    - Working on retrieve stepgoals

---
+++ maui/Components/Models/StepgoalModel.cs
@@ -0,0 +1,15 @@
+﻿// BlogModel.cs
+
+using System.Runtime.InteropServices.JavaScript;
+
+namespace maui.Components.Models
+{
+    public class StepgoalModel
+    {
+        public int Goal{ get; set; }
+        public DateOnly Date { get; set; }
+        public string? Progress { get; set; }
+        public string? Achieved { get; set; }
+        //public int Counter { get; set; } // New property to hold the counter
+    }
+}
---
+++ maui/Components/ViewModels/StepgoalViewModel.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+using maui.Components.Models;
+
+public class StepgoalViewModel : INotifyPropertyChanged
+{
+    private readonly HttpClient _httpClient;
+
+    public event PropertyChangedEventHandler PropertyChanged;
+
+    public StepgoalViewModel(HttpClient httpClient)
+    {
+        _httpClient = httpClient;
+        StepGoals = new List<StepgoalModel>();
+        LoadStepGoals();
+    }
+
+    private List<StepgoalModel> _stepgoals;
+    public List<StepgoalModel> Stepgoals
+    {
+        get => _stepgoals;
+        set
+        {
+            _stepgoals = value;
+            OnPropertyChanged(nameof(_stepgoals));
+        }
+    }
+
+    private async Task LoadStepGoals()
+    {
+        try
+        {
+            var blogs = await _httpClient.GetFromJsonAsync<IEnumerable<BlogModel>>("http://localhost:5041/api/blogs");
+            if (blogs != null)
+                Stepgoals = Stepgoals.ToList();
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Error: {ex.Message}");
+            // Handle error
+        }
+    }
+
+
+    protected virtual void OnPropertyChanged(string propertyName)
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+}
